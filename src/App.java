f77c4666dd91458bbfe6e14372eb9288814f175e
Hodei Mayoral <ikcei@plaiaundi.net>
2024-12-11 10:52:02 +0100

    commit ruina

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, ander!");
+        System.out.println("Hello, stompy!");
+        System.out.println("Hola mundo");
     }
 }
