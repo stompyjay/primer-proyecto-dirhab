31f20948b31f8116d5004905758ac7037d9a9bdc
Ander <ikdgj@plaiaundi.net>
2024-12-11 10:51:13 +0100

    lokura

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, ander!");
+        System.out.println("Hello, latienespequeña!");
+        System.out.println("paranoya!");
+        
     }
 }
